94465757da827677063611a3fcfc6dd3af07653e
Gonçalo Reis <goncalomanuelreis@gmail.com>
2020-08-15 23:48:42 +0100

    Update 29. weather_data.py

--- 29. weather_data.py
+++ 29. weather_data.py
@@ -1,6 +1,6 @@
 import json, requests
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=1ccc384606a371dcdd78875e7b6174e3'
+url = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=********'
 site = requests.get(url)
 info = json.loads(site.text)
 print(f"Temperature is {info['main']['temp']}")
